308be441fdf8ef3fcf53bb92da3a12bc465f4dff
vaibhav Dwivedi <vaibhavdwivedi3212gmail.com>
2022-10-30 23:03:36 +0530

    Updates

---
+++ precache-manifest.da79624919e1ee7c2af8f5d2c40dcf53.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fc4c03f9e2be49de4904f40f1345ce",
+    "url": "/NotesKeeper/index.html"
+  },
+  {
+    "revision": "c8e8b58a8544b544c4eb",
+    "url": "/NotesKeeper/static/js/2.2eed730b.chunk.js"
+  },
+  {
+    "revision": "8209e22450c0ef73b8ad",
+    "url": "/NotesKeeper/static/js/main.ee76e9d4.chunk.js"
+  },
+  {
+    "revision": "49a54b89798684a2da79",
+    "url": "/NotesKeeper/static/js/runtime-main.cae41cae.js"
+  }
+]);
