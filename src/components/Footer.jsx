1e59d0857696a1be85bb5ae1784cfbb308314271
vaibhav Dwivedi <vaibhavdwivedi3212gmail.com>
2022-10-30 23:00:31 +0530

    Deployed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Vaibhav Dwivedi's Copyright ⓒ {year}</p>
     </footer>
   );
 }
